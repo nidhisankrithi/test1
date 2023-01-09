c252d5aa427f765c677f15b4a4a2ae0819cc5e36
nidhisankrithi <nidhisankruthi@gmail.com>
2023-01-09 11:34:05 +0530

    Print statement changed from 'Entet the number of elements to enter the elements'

---
+++ Main_Method.java
@@ -0,0 +1,11 @@
+
+public class Main_Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		largest_and_smallest obj=new largest_and_smallest();
+		obj.largestand_smallest();
+
+	}
+
+}
---
+++ largest_and_smallest.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class largest_and_smallest {
+	public void largestand_smallest()
+	{
+int n,min,max;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number of elements in an array");
+ n=sc.nextInt();
+ int a[]=new int[n];
+ System.out.println("Enter the number of elements in an array");
+ for(int i=0;i<n;i++)
+ {
+	 a[i]=sc.nextInt();
+	 
+ }
+ System.out.println("Array Elements are ");
+ for(int i=0;i<n;i++)
+ {
+	 
+	 System.out.println(a[i]);
+	 
+ }
+ max=a[0];
+ min=a[0];
+ 
+ for(int i=0;i<n;i++)
+ {
+	 
+	 if(a[i]>max)
+	 {
+		 max=a[i];
+	 }
+	 
+ }
+ System.out.println("Max value in an array is:" +max);
+ 
+ 
+ for(int i=0;i<n;i++)
+ {
+	 
+	 if(a[i]<min)
+	 {
+		 min=a[i];
+	 }
+	 
+ }
+ System.out.println("Max value in an array is:" +min);
+	}
+}
